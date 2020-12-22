1b7ce28f062c50f838473230f83db9f0f67a2814
Jong9106 <juank_hr@hotmail.com>
2020-12-22 02:42:14 -0500

    delete functions

--- functions.c
+++ functions.c
@@ -54,93 +54,20 @@ void _pall(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * free_list - Free memory from a list
- * @stack: pointer to head node
- */
-void free_list(stack_t *stack)
-{
-	stack_t *temp;
-
-	while (stack)
-	{
-		temp = stack;
-		stack = stack->next;
-		free(temp);
-	}
-}
-
-/**
- * check_number - Free memory from a list
- * @data: pointer to head node
+ * _pint - function to add a node at head
+ * @stack: pointer to head pointer
  * @line_number: line from monty file
- * Return: data into integer
  */
-int check_number(char *data, unsigned int line_number)
+void _pint(stack_t **stack, unsigned int line_number)
 {
-	int i = 0;
-
-	if (data == NULL)
+	if (!*stack)
 	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		free(break_free.buf);
-		free_list(break_free.list_head);
-		fclose(break_free.filedes);
-		exit(EXIT_FAILURE);
-	}
-
-	if (data[i] == '-')
-		i++;
-	for (; data[i] != '\0' ; i++)
-	{
-		if (data[i] < '0' || data[i] > '9')
-		{
-			fprintf(stderr, "L%u: usage: push integer\n", line_number);
-			free(break_free.buf);
-			free_list(break_free.list_head);
-			fclose(break_free.filedes);
-			exit(EXIT_FAILURE);
-		}
-	}
-	return (atoi(data));
-}
-
-/**
- *get_f - output format
- *@line_number: line number from monty file
- */
-void get_f(unsigned int line_number)
-{
-	int same = 0;
-	char *opcode = NULL;
-	unsigned int j = 0;
-
-	instruction_t options[] = {
-		{"push", _push},
-		{"pall", _pall},
-		{NULL, NULL}
-	};
-
-	opcode = strtok(break_free.buf, " ");
-
-	if (opcode)
-	{
-		for (j = 0 ; options[j].opcode ; j++)
-		{
-			same = strcmp(options[j].opcode, opcode);
-			if (same == 0)
-			{
-				options[j].f(&break_free.list_head, line_number);
-				break;
-			}
-		}
-	}
-	if (options[j].opcode == NULL)
-	{
-		fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		free(break_free.buf);
 		if (break_free.list_head)
 			free_list(break_free.list_head);
 		fclose(break_free.filedes);
 		exit(EXIT_FAILURE);
 	}
+	printf("%d\n", (*stack)->n);
 }
