1d3f64b3e675e0b0d0e3d65a0a2676b8361cd0b1
Jong9106 <juank_hr@hotmail.com>
2020-12-22 04:26:30 -0500

    Added sub function

---
+++ functions3.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * _sub - function to sub data from nodes
+ * @stack: pointer to head pointer
+ * @line_number: line from monty file
+ */
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (!(*stack) || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		free(break_free.buf);
+		if (break_free.list_head)
+			free_list(break_free.list_head);
+		fclose(break_free.filedes);
+		exit(EXIT_FAILURE);
+	}
+
+	temp->next->n = (*stack)->next->n - (*stack)->n;
+	(*stack) = temp->next;
+
+	free(temp);
+
+}
