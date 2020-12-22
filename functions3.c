213c545d0333ec16c7c79b6485367e6f06970123
Jong9106 <juank_hr@hotmail.com>
2020-12-22 04:42:52 -0500

    add more function

--- functions3.c
+++ functions3.c
@@ -86,3 +86,49 @@ void _mul(stack_t **stack, unsigned int line_number)
 	free(temp);
 
 }
+
+/**
+ * _mod - function to  divide data from nodes
+ * @stack: pointer to head pointer
+ * @line_number: line from monty file
+ */
+void _mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (!(*stack) || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		free(break_free.buf);
+		if (break_free.list_head)
+			free_list(break_free.list_head);
+		fclose(break_free.filedes);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		free(break_free.buf);
+		if (break_free.list_head)
+			free_list(break_free.list_head);
+		fclose(break_free.filedes);
+		exit(EXIT_FAILURE);
+	}
+
+	temp->next->n = (*stack)->next->n / (*stack)->n;
+	(*stack) = temp->next;
+
+	free(temp);
+
+}
+
+/**
+ * _coment - function to  divide data from nodes
+ * @stack: pointer to head pointer
+ * @line_number: line from monty file
+ */
+void _coment(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
