3688ad652fd4aa29581216fa4b98709e140eaa13
Jong9106 <juank_hr@hotmail.com>
2020-12-22 21:45:38 -0500

    Wrong error output

--- functions4.c
+++ functions4.c
@@ -32,3 +32,38 @@ void _pchar(stack_t **stack, unsigned int line_number)
 	}
 
 }
+
+/**
+ * _pstr - function to print string from node
+ * @stack: pointer to head pointer
+ * @line_number: line from monty file
+ */
+void _pstr(stack_t **stack, unsigned int line_number)
+{
+	char word = 0;
+
+	word += (*stack)->n;
+
+	if (!*stack)
+	{
+		putchar(10);
+	}
+
+	if ((*stack)->n < 0 || (*stack)->n >= 127)
+	{
+		fprintf(stderr, "L%u: can't pstr, value out of range\n", line_number);
+		free(break_free.buf);
+		if (break_free.list_head)
+			free_list(break_free.list_head);
+		fclose(break_free.filedes);
+		exit(EXIT_FAILURE);
+	}
+
+	else
+	{
+		putchar(word);
+		putchar(10);
+
+	}
+
+}
